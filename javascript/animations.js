dc0b387ce3fa1318b049410694ebce489576bef2
Programmer-Nathan <82857078+Programmer-Nathan@users.noreply.github.com>
2024-07-05 16:49:16 +1200

    Additional Code Commenting
    I added a few more lines of comments to show an understanding of my code.

--- javascript/animations.js
+++ javascript/animations.js
@@ -9,3 +9,10 @@ const observer = new IntersectionObserver((entries) => {
   
   const hiddenElements = document.querySelectorAll('.hidden');
   hiddenElements.forEach((el) => observer.observe(el))
+
+  /*
+  This part of the code is an intersection oberserver
+  An intersection observer detects when an element, in this case, all elements with the class 'hidden' are visible on screen
+  It then targets these elements and adds the 'show' class
+  Any elements with this 'show' class are animated with a transition
+*/
